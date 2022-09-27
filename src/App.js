3ff206575391996fea66d8942a53349fe24a12ab
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2022-09-27 10:54:26 +0500

    basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Recipe from "./pages/Recipe";
 
 function App() {
     return (
-        <Router>
+        <Router basename='/react-magazine'>
             <Header/>
             <main className="content container">
                 <Routes>
